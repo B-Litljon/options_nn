1bd8fe022d7bd99fa11b1bcfa37fb1651c09abd5
B-litljon <brandonc.littlejohn@gmail.com>
2024-06-24 00:25:45 -0700

    add screening methods to the DataCollector class

--- data/collection/alpaca_data.py
+++ data/collection/alpaca_data.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 import alpaca
+from alpaca.data.historical.screener import ScreenerClient
 from alpaca.data.live.stock import StockDataStream
 from alpaca.data.historical.stock import (
     StockHistoricalDataClient,
@@ -27,7 +28,30 @@ from alpaca.common.exceptions import APIError
 
 
 class DataCollector:
-    def __init__(self, api_key, secret_key):
+    def __init__(self, api_key, secret_key, symbol, timeframe, days_back):
         self.historical_client = StockHistoricalDataClient(api_key, secret_key)
-
+        self.historical_option_client = OptionHistoricalDataClient(api_key, secret_key)
+        self.asset_screener = ScreenerClient(api_key, secret_key)
+        self.live_client = StockDataStream(api_key, secret_key)
+        self.option_live_client = OptionDataStream(api_key, secret_key)
+        self.symbol = symbol
     
+
+    # we can use the screener api to filter for the most active stocks or market movers
+    # then return a dictionary of whichever stocks we like
+    # with the symbol as the key, and the ohlcv data as the values 
+    # then we can request the options chain data for each stock in the dictionary
+
+    def get_stock_data(self, symbol, timeframe, days_back):
+        end = datetime.now(ZoneInfo("America/Pacific"))
+        start = end - timedelta(days=days_back)
+        data = StockBarsRequest(symbol, timeframe, start, end)
+        return self.historical_client.get_bars(data)
+    
+    def most_active_stocks(self):
+        data = self.asset_screener.get_most_actives()
+        return data
+    
+    def market_movers(self):
+        data = self.asset_screener.get_market_movers()
+        return data
